6dc465b16a447c286b007499b0588075a03013ef
Stanisław Synowiec <stasyn1410@gmail.com>
2023-02-17 22:23:44 +0100

    new icons

--- src/components/variantOption/variantOption.tsx
+++ src/components/variantOption/variantOption.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import Image from 'next/image';
 
 type VariantOptionProps = {
 	name: string;
@@ -9,9 +10,15 @@ export const VariantOption = ({
 	name,
 	contains = true,
 }: VariantOptionProps) => {
+	const containsIcons = contains ? (
+		<Image src="/icons/check.png" alt="" width="20" height="20" />
+	) : (
+		<Image src="/icons/x.png" alt="" width="15" height="15" />
+	);
+
 	return (
-		<div className="flex pt-5">
-			<span className="pr-2">{contains ? '✅' : '❌'}</span>
+		<div className="flex items-center pt-5">
+			<span className="pr-2">{containsIcons}</span>
 			<p className={clsx(contains ? 'text-black' : 'text-gray-400')}>{name}</p>
 		</div>
 	);
