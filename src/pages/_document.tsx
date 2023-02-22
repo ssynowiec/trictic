44d8684465443d915d731b204ee9864b3011198d
Stanisław Synowiec <stasyn1410@gmail.com>
2023-02-22 12:02:36 +0100

    add: SEO & meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,45 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang="pl" className="scroll-smooth">
-			<Head />
+			<Head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="description" content="Trictic" />
+				<meta name="author" content="@ssynowiec" />
+				<meta name="keywords" content="Trictic" />
+
+				<meta name="title" content="Trictic" />
+				<meta
+					name="description"
+					content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+				/>
+
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://trictic.vercel.app/" />
+				<meta property="og:title" content="Trictic" />
+				<meta
+					property="og:description"
+					content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+				/>
+				<meta
+					property="og:image"
+					content="https://trictic.vercel.app/trictic.png"
+				/>
+
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content="https://trictic.vercel.app/" />
+				<meta property="twitter:title" content="Trictic" />
+				<meta
+					property="twitter:description"
+					content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+				/>
+				<meta
+					property="twitter:image"
+					content="https://trictic.vercel.app/trictic.png"
+				/>
+
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,14 @@ import { Hero } from 'components/hero/hero';
 import { PriceList } from 'components/priceList/priceList';
 import { Functions } from 'components/functions/functions';
 import { Aboutus } from 'components/aboutus/aboutus';
+import Head from 'next/head';
 
 const Home: NextPageWithLayout = () => {
 	return (
 		<>
+			<Head>
+				<title>Trictic</title>
+			</Head>
 			<Hero />
 			<Functions />
 			<PriceList />
